9d569734b97c21f8116a6809ca727a4d7ffb5395
RenMuming <1652339654@qq.com>
2026-04-01 00:27:11 +0800

    chore: vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { dirname, resolve } from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
-import { resolve, dirname } from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-	plugins: [vue()],
-	envDir: resolve(__dirname, 'env'),
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-		},
-	},
+  plugins: [vue()],
+  envDir: resolve(__dirname, 'env'),
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
